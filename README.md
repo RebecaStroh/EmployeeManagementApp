faf872bed63aa5d38081894d6d2359ffa505625b
Rebeca Stroh <rebecapstroh@gmail.com>
2023-11-18 15:21:30 -0300

    [FE] Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # EmployeeManagementApp
 Projeto React/Node.js/MongoDB para gerenciar dados de funcionários.
+
+# Configuring the environment
+
+
+## Frontend
+
+Make sure the following versions are available:
+node v18.17.0
+npm 9.6.7
+
+After having it all installed, acess /frontend/employee-management-app and run
+npm install
+
+Now that we have it configured, start it:
+npm start
---
+++ frontend/employee-management-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Welcome to Employee Management App! </h1>
+        <p>Simplifying onboarding and streamline HR processes.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
