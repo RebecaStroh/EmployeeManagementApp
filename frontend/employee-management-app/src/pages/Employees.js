633e6ab49d6b9d1cd3a9034fd0b3557df72405c3
Rebeca Stroh <rebecapstroh@gmail.com>
2023-11-24 01:09:20 -0300

    [FE] Update read me and add local urls

--- frontend/employee-management-app/src/pages/Employees.js
+++ frontend/employee-management-app/src/pages/Employees.js
@@ -20,6 +20,8 @@ function Employees() {
   const fetchEmployees = async () => {
     setLoading(true);
     try {
+      // Change to specific environment
+      // const response = await fetch('http://127.0.0.1:5001/employeemanagementapp-767af/us-central1/getAllEmployees', {
       const response = await fetch('https://getallemployees-nlxluegtta-uc.a.run.app', {
         method: 'POST',
         headers: {
